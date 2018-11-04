f9d2f5120616252324320995431bc98620d2d168
Zoli <mr.zoltan.takacs@gmail.com>
2018-11-04 18:02:55 +0100

    day 2 work

---
+++ week_02/day_2/src/Doubling.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int baseNum = 123;
+
+        doubling(baseNum);
+    }
+
+
+        public static void doubling ( int baseNum){
+            int sum = baseNum * 2;
+            System.out.println(sum);
+
+    }
+}
